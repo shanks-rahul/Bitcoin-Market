f35ad6175cf91b892e288c21c4e4df26341b43e1
shanks-rahul <rahulagarwalla1234@gmail.com>
2024-04-30 09:14:28 +0530

    just rendered the HomeLayout Page

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import HomeLayout from './Layouts/HomeLayout'
+import YouMayAlsoLike from './Pages/YouMayAlsoLike'
+import FundamentalPage from './Pages/FundamentalPage'
+import SentimentsPage from './Pages/SentimentsPage'
+
+function App() {
+  
+
+  return (
+    <>
+     <HomeLayout/>
+    </>
+  )
+}
+
+export default App
