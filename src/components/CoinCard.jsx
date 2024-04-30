4041b9c81c374993ee2d9257161e12c91fd94dc2
shanks-rahul <rahulagarwalla1234@gmail.com>
2024-04-30 08:57:13 +0530

    added the cards structure

---
+++ src/components/CoinCard.jsx
@@ -0,0 +1,28 @@
+import {
+    Card,
+    CardBody,
+    CardFooter,
+    Typography,
+    Button,
+} from "@material-tailwind/react";
+
+function SimpleCard({ data}) {
+    return (
+        <div className="flex">
+            <div className="bg-white rounded-3xl border-[1px] border-black shadow-xl p-8 m-2 w-[10.4rem] h-[10rem]">
+                <div className="flex">
+                    <button className="inline-flex items-center justify-center w-8 h-8 rounded-[50%] text-blue-100 bg-gradient-to-r">
+                        <img className="text-xs" src={data.item.small} alt="" />
+                        <span className=" ml-1 font-medium text-xs text-gray-500 flex justify-end">{data.item.name}</span>
+                    </button>
+                </div>
+                <div className="flex flex-col items-start p-4">
+                    <h3 className="font-semibold text-sm mb-2 text-gray-800">{data.item.data.market_cap}</h3>
+                    <img src={data.item.data.sparkline} alt="" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SimpleCard;
