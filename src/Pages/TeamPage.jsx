d75e9e856a7c8375cf5b8cf8b224ab865be06447
shanks-rahul <rahulagarwalla1234@gmail.com>
2024-04-30 09:02:42 +0530

    added the Team Section Of the Page

---
+++ src/Pages/TeamPage.jsx
@@ -0,0 +1,41 @@
+function TeamPage(){
+    return(
+        <div className="flex flex-col space-y-2">
+            <h1 className="font-bold text-xl ">Team</h1>
+            <p className="text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, maiores deserunt nam perferendis accusantium dolore earum illum. Laborum, neque qui!</p>
+            <div className="flex flex-col space-y-2">
+                <div className="flex flex-wrap space-x-4 items-center p-3 bg-blue-100 rounded-md sm:flex justify-center">
+                    <div className="text-center sm:w-[13%] lg:w-[9%] ">
+                        <img className="rounded-md" src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                        <p className="text-sm font-semibold">John Smith</p>
+                        <p className="text-gray-400 text-sm">Designation here</p>
+                    </div>
+                    <div className="w-[89%]">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad at cupiditate nisi illum odit voluptatem perferendis dolorum magnam ab molestiae? Laboriosam molestiae error iste perspiciatis ipsa! Id facilis quis pariatur, sunt voluptates odit dicta quos possimus earum eaque dolores praesentium Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae enim cum nihil ex consectetur sed dolor dolorem. Quidem, fuga alias!.</p>
+                    </div>
+                </div>
+                <div className="flex flex-wrap space-x-4 items-center p-3 bg-blue-100 rounded-md sm:flex justify-center">
+                    <div className="text-center sm:w-[13%] lg:w-[9%] ">
+                        <img className="rounded-md" src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <p className="text-sm font-semibold">Elina Williams</p>
+                        <p className="text-gray-400 text-sm">Designation here</p>
+                    </div>
+                    <div className="w-[89%]">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad at cupiditate nisi illum odit voluptatem perferendis dolorum magnam ab molestiae? Laboriosam molestiae error iste perspiciatis ipsa! Id facilis quis pariatur, sunt voluptates odit dicta quos possimus earum eaque dolores praesentium Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda deleniti accusantium sed esse nostrum voluptatibus odit voluptatum dignissimos iste repellat!.</p>
+                    </div>
+                </div>
+                <div className="flex flex-wrap space-x-4 items-center p-3 bg-blue-100 rounded-md sm:flex justify-center">
+                    <div className="text-center sm:w-[13%] lg:w-[9%] ">
+                        <img className="rounded-md" src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
+                        <p className="text-sm font-semibold">John Smith</p>
+                        <p className="text-gray-400 text-sm">Designation here</p>
+                    </div>
+                    <div className="w-[89%]">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad at cupiditate nisi illum odit voluptatem perferendis dolorum magnam ab molestiae? Laboriosam molestiae error iste perspiciatis ipsa! Id facilis quis pariatur, sunt voluptates odit dicta quos possimus earum eaque dolores praesentium Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero laudantium, cumque nam repudiandae deleniti quia quis saepe error. Voluptatum, laboriosam..</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default TeamPage;
